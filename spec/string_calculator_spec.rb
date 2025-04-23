d85edfc409981fe3abd8ba81d653d9c10e44650e
Nilesh Sharma <snilesh97@gmail.com>
2025-04-23 12:41:13 +0530

    Step 6: Support custom delimiters like //;\n1;2

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,5 +23,9 @@ RSpec.describe StringCalculator do
     it 'handles newlines as delimiters along with commas' do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports custom delimiters defined in the format //;\n1;2' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -4,6 +4,13 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2])
+      numbers = parts[1]
+    end
+
+    numbers.split(/#{delimiter}|,|\n/).map(&:to_i).sum
   end
 end
