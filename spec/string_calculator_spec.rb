5cede13c46f1b1d856ac0e14f7bf4fad05dd04a7
Nilesh Sharma <snilesh97@gmail.com>
2025-04-23 12:15:22 +0530

    Step 1: Return 0 for empty string

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative '../string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    it 'returns 0 for empty string' do
+      expect(StringCalculator.add('')).to eq(0)
+    end
+  end
+end
---
+++ string_calculator.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class StringCalculator
+  def self.add(numbers)
+    0 if numbers.empty?
+  end
+end
