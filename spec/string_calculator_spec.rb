af00ecec671b8b4e80d5d3ebb6c78724d4884d4f
Nilesh Sharma <snilesh97@gmail.com>
2025-04-23 12:28:09 +0530

    Step 4: Support sum of any number of comma-separated numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -15,5 +15,9 @@ RSpec.describe StringCalculator do
     it 'returns the sum of two comma-separated numbers' do
       expect(StringCalculator.add('1,2')).to eq(3)
     end
+
+    it 'returns the sum of multiple comma-separated numbers' do
+      expect(StringCalculator.add("1,2,3,4")).to eq(10)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -4,10 +4,6 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    if numbers.include?(',')
-      numbers.split(',').map(&:to_i).sum
-    else
-      numbers.to_i
-    end
+    numbers.split(",").map(&:to_i).sum
   end
 end
